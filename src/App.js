f9bd46ecff94683f9304bc77417b8baa532000e7
maxgrier <max.grier1@gmail.com>
2024-01-07 13:41:45 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Slider from './components/Slider/Slider'
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Slider/>
     </div>
   );
 }
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,44 @@
+// https://github.com/IamDyroz/carousel/blob/main/src/components/sliderImage.js
+
+import React, { useEffect, useState } from "react";
+import SliderContent from "../SliderContent/SliderContent";
+import Dots from "../Dots/Dots";
+import Arrows from "../Arrows/Arrows.jsx";
+import sliderImage from "../SliderImage/sliderImage";
+// import sliderImage from "./sliderImage"
+import "./Slider.css";
+
+const len = sliderImage.length - 1;
+
+function Slider(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex(activeIndex === len ? 0 : activeIndex + 1);
+    }, 5000);
+// }, 50000);
+    return () => clearInterval(interval);
+  }, [activeIndex]);
+
+  return (
+    <div className="slider-container">
+      <SliderContent activeIndex={activeIndex} sliderImage={sliderImage} />
+      <Arrows
+        prevSlide={() =>
+          setActiveIndex(activeIndex < 1 ? len : activeIndex - 1)
+        }
+        nextSlide={() =>
+          setActiveIndex(activeIndex === len ? 0 : activeIndex + 1)
+        }
+      />
+      <Dots
+        activeIndex={activeIndex}
+        sliderImage={sliderImage}
+        onclick={(activeIndex) => setActiveIndex(activeIndex)}
+      />
+    </div>
+  );
+}
+
+export default Slider;
