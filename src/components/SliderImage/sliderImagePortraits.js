3a977091be6ceb15d8f60112a8c1e76477c23cc2
maxgrier <max.grier1@gmail.com>
2024-06-09 21:57:27 -0700

    Adding portraits rebase2 again2

--- src/components/SliderImage/sliderImagePortraits.js
+++ src/components/SliderImage/sliderImagePortraits.js
@@ -1,5 +1,6 @@
 import First from "../../assets/images/portraits/DSC00045.jpg";
 import Second from "../../assets/images/portraits/DSC00126.jpg";
+import Third from "../../assets/images/portraits//new/DSC00013.jpg";
 // import Third from "../../assets/DSC00717.jpg";
 
 export default [
@@ -20,13 +21,85 @@ export default [
     name2: "DSC00126",
   },
   {
-    title: "Second Slide",
+    title: "Third Slide",
     description: "This is the second slider Image of our carousel",
-    urls: Second,
+    urls: Third,
     layout: 'portriat',
-    name: "DSC00126.jpg",
-    name2: "DSC00126",
+    name: "DSC00013.jpg",
+    name2: "DSC00013",
   },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00056.jpg"),
+    layout: 'portriat',
+    name: "DSC00056.jpg",
+    name2: "DSC00056",
+  },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00060.jpg"),
+    layout: 'portriat',
+    name: "DSC00060.jpg",
+    name2: "DSC00060",
+  },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00140.jpg"),
+    layout: 'portriat',
+    name: "DSC00140.jpg",
+    name2: "DSC00140",
+  },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00145.jpg"),
+    layout: 'portriat',
+    name: "DSC00145.jpg",
+    name2: "DSC00145",
+  },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00149.jpg"),
+    layout: 'portriat',
+    name: "DSC00149.jpg",
+    name2: "DSC00149",
+  },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00156.jpg"),
+    layout: 'portriat',
+    name: "DSC00156.jpg",
+    name2: "DSC00156",
+  },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00158.jpg"),
+    layout: 'portriat',
+    name: "DSC00158.jpg",
+    name2: "DSC00158",
+  },
+  {
+    title: "",
+    description: "",
+    urls: require("../../assets/images/portraits/new/DSC00159.jpg"),
+    layout: 'portriat',
+    name: "DSC00159.jpg",
+    name2: "DSC00159",
+  },
+  // {
+  //   title: "",
+  //   description: "",
+  //   urls: Second,
+  //   layout: 'portriat',
+  //   name: "DSC00056.jpg",
+  //   name2: "DSC00056",
+  // },
   // {
   //   title: "Third Slide",
   //   description: "This is the Third slider Image of our carousel",
